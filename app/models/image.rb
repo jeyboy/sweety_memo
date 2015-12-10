74545518dcb9f75e73f38ffe250a953e9d3006ca
Jenua Boiko <jeyboy@bigmir.net>
2015-12-10 23:01:28 +0200

    add validation on image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,13 @@ class Image < ActiveRecord::Base
   mount_uploader :file, ImageUploader
 
   belongs_to :imageable, polymorphic: true
+
+  validate :image_size_validation, if: 'file?'
+
+
+  private
+
+  def image_size_validation
+    errors.add(:base, 'Image should be less than 1MB') if file.size > 1.megabytes
+  end
 end
