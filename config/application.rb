10e4b65bee1ca8e3523ec5ea28ba18f13330223c
Jenua Boiko <jeyboy@bigmir.net>
2015-12-11 23:41:56 +0200

    updates for translations

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module SweetyMemo
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
 
     config.assets.precompile += %w( panel.css )
     config.assets.precompile += %w( panel.js )
