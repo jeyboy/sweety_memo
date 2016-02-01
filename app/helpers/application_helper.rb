8b484e8372379c53a5deb709433bcaaccce41063
Jenua Boiko <jeyboy@bigmir.net>
2016-02-01 04:38:19 +0200

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def image(obj)
+    image_tag((obj.image.file.thumbnail.url rescue ImageUploader::default_url), height: '100', class: 'img-rounded').html_safe
+  end
+
+  def async_image(obj)
+    image_tag('#', class: 'img-rounded async_img', data: {src: (obj.image.file.standard.url rescue ImageUploader::default_url)}).html_safe
+  end
 end
