0e1b0fd7e42182315b9bdb9f78f568c133a3391d
Jenua Boiko <jeyboy@bigmir.net>
2016-02-01 02:37:59 +0200

    improvements

--- app/models/gallery_topic.rb
+++ app/models/gallery_topic.rb
@@ -1,7 +1,10 @@
 class GalleryTopic < ActiveRecord::Base
+  has_one :image, as: :imageable
   has_many :gallery_items, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 254}
 
   scope :enabled, -> { where(disabled: false) }
+
+  accepts_nested_attributes_for :image, reject_if: ->(attributes) { attributes['file'].blank? }
 end
