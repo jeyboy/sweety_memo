17e3ad4986609862e20bea81edd1df0cd4e93078
Jenua Boiko <jeyboy@bigmir.net>
2016-02-14 17:34:17 +0200

    improve redirecting

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,5 @@ Rails.application.routes.draw do
   post 'search', to: 'home#search', as: :search
 
   root 'home#show'
-  match '*path' => redirect('/'), via: [:get, :post]
+  match '*path' => redirect { |p, req| req.flash[:alert] = 'Aaargh, this path is not exist'; '/' }, via: [:get, :post]
 end
