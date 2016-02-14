a05322d2ecf45e883fadac13fce2176822c143cd
Jenua Boiko <jeyboy@bigmir.net>
2016-02-14 19:13:30 +0200

    updates for history

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,42 +6,50 @@
 //= require weather/index
 //= require_self
 
+window.update_page = function(url) {
+    $content = $('#main_content');
+    img_url = $content.attr('data-loading-img');
+    $content.html("<img src='" + img_url + "' height='100px' style='display: block; margin: auto;' />");
+
+    if (/\/$/.test(url))
+        url += '?format=json';
+    else
+        url += '.json';
+
+    $.ajax({
+        url: url,
+        method: $this.attr('data-method') || 'get',
+        success: function (response) {
+            history.pushState(null, document.title, window.last_location);
+            window.last_location = url;
+
+            $content.html(response.content);
+            async_image_loading();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $content.html("<h2>Sorry, but some shit happened :(<h2><p>" + errorThrown + "</p>>");
+        }
+    });
+};
+
 window.last_location = location.pathname;
+
+window.onpopstate = function(event) {
+    alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
+    update_page(document.location, true);
+};
 
 $('document').ready(function() {
     $('body').on('click', 'a:not(.direct)', function() {
         $this = $(this);
-        def_url = url = $this.attr('href');
+        url = $this.attr('href');
 
         if (url != '#' && url != window.last_location) {
             $this.closest('.dropdown.open').removeClass('open');
             if ($(".navbar-toggle:visible").length > 0)
                 $(".navbar-collapse.in").collapse('hide');
 
-            $content = $('#main_content');
-            img_url = $content.attr('data-loading-img');
-            $content.html("<img src='" + img_url + "' height='100px' style='display: block; margin: auto;' />");
-
-            if (/\/$/.test(url))
-                url += '?format=json';
-            else
-                url += '.json';
-
-            $.ajax({
-                url: url,
-                method: $this.attr('data-method') || 'get',
-                success: function (response) {
-                    history.replaceState(null, document.title, def_url);
-                    history.pushState(null, document.title, window.last_location);
-                    window.last_location = def_url;
-
-                    $content.html(response.content);
-                    async_image_loading();
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    $content.html("<h2>Sorry, but some shit happened :(<h2><p>" + errorThrown + "</p>>");
-                }
-            });
+            update_page(url);
 
             return false;
         }
